9dc716601e55d0312a8a435b40bd4f14e6f319f6
twomeetings <twomeetings@gmail.com>
2018-05-16 15:08:04 +0800

    feature: completed config and ignore gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,44 @@ var through = require('through')
 var through2 = require('through2')
 var map = require('vinyl-map')
 
+const compiledFolder = '_dist/'
+
 gulp.task('config', function() {
-	const updateConfig = through2.obj(function(chunk, env, callback){
-		console.log('===== chunk:', chunk)
-		console.log('===== env:', env)
-		console.log('===== callback:', callback)
-	})
+  const updateConfig = through2.obj(function(chunk, env, callback) {
+    let content = chunk.contents.toString()
+    let json = JSON.parse(content)
+    json.miniprogramRoot = compiledFolder
+
+    chunk.contents = Buffer.from(JSON.stringify(json))
+
+    this.push(chunk)
+    callback()
+  })
+
   return gulp
     .src('./project.config.json')
-		.pipe(updateConfig)
+    .pipe(updateConfig)
     .pipe(prettify({ indent_size: 2 }))
     .pipe(gulp.dest('./'))
 })
 
-gulp.task('file', function() {
-  const sourceFilePath = '.gitignore'
+gulp.task('ignore', function() {
+  const sourceFilePath = './.gitignore'
   if (!fs.existsSync(sourceFilePath)) {
-    fs.appendFileSync(sourceFilePath, '')
+    fs.appendFileSync(sourceFilePath, compiledFolder)
   }
 
-  return gulp.src(sourceFilePath).pipe(gulp.dest('./'))
+  const addCompiledDirToGitIgnore = through2.obj(function(chunk, env, cb) {
+    let contents = chunk.contents.toString()
+    if (contents.indexOf(compiledFolder) <= -1) {
+      var os = require('os')
+      fs.appendFileSync(sourceFilePath, os.EOL)
+      fs.appendFileSync(sourceFilePath, compiledFolder)
+    }
+  })
+
+  return gulp
+    .src(sourceFilePath)
+    .pipe(addCompiledDirToGitIgnore)
+    .pipe(gulp.dest('./'))
 })
