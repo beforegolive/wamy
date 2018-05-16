695c8acb9ea1d2d5c4b9d547cad6c43c15627b9d
Xu Lianghui <twomeetings@gmail.com>
2018-05-16 12:06:21 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@ var fs = require('fs')
 var gulp = require('gulp')
 var prettify = require('gulp-jsbeautifier')
 var through = require('through')
-
-const editConfig = through()
+var through2 = require('through2')
+var map = require('vinyl-map')
 
 gulp.task('config', function() {
+	const updateConfig = through2.obj(function(chunk, env, callback){
+		console.log('===== chunk:', chunk)
+		console.log('===== env:', env)
+		console.log('===== callback:', callback)
+	})
   return gulp
     .src('./project.config.json')
+		.pipe(updateConfig)
     .pipe(prettify({ indent_size: 2 }))
     .pipe(gulp.dest('./'))
 })
